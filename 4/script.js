9d6fe739449d1b4f120b62ae2a033635243651c9
Antoine Pascual <74872926+Flashtheorie@users.noreply.github.com>
2022-03-29 21:25:37 +0200

    Exercice 4 terminé

---
+++ 4/script.js
@@ -0,0 +1,11 @@
+// 4°:  Calculate the sum of numbers within an array
+
+
+let array = [1, 2, 3, 4];
+let a = 0;
+
+for (let i = 0; i < array.length; i++) {
+    a += array[i];
+}    
+
+document.write(a);
