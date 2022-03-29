7d1f95e5a260aa932542aa2aac151635626136d3
Antoine Pascual <74872926+Flashtheorie@users.noreply.github.com>
2022-03-29 21:29:05 +0200

    Exercice 5 terminé

---
+++ 5/script.js
@@ -0,0 +1,12 @@
+// 5° : Create a function that reverses an array
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function reverse(nomDeLarray) {
+    let reverseArray = nomDeLarray.reverse();
+    document.write(reverseArray);
+}
+
+reverse(array);
+
+
